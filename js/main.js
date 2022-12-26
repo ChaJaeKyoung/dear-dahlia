f441e50b3685dea9a91512d23c280492499f6cce
jenny.cha <chajg91@naver.com>
2022-12-26 18:45:22 +0900

    6. day24-con1-dahlia's-pick
    js추가

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ btnPrev.addEventListener('click', function () {
 });
 
 //dahlia's pick 토글
+const dahliaPickToggleEls = document.querySelectorAll('.dahlia-pick__titles h3');
+
+dahliaPickToggleEls.forEach(function(dahliaPickToggleEl, index) {
+  dahliaPickToggleEl.addEventListener('click', function () {
+    dahliaPickToggleEls.forEach(function (pickEl) {
+      pickEl.classList.remove('active');
+    })
+    dahliaPickToggleEl.classList.add('active');
+    // if(dahliaPickToggleEl.classList.contains('active')!=true) {
+    //   dahliaPickToggleEl.classList.remove('active');
+    // }
+
+    document.querySelectorAll('.pick-' + (index + 1));
+  });
+})
