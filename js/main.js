ebfd05383f5d520234420befdaf4445291087757
jenny.cha <chajg91@naver.com>
2022-12-27 14:17:36 +0900

    7.day25-con1-dahlia's-pick
    js로 tab구현하기

--- js/main.js
+++ js/main.js
@@ -4,19 +4,33 @@ btnPrev.addEventListener('click', function () {
   console.log('click');
 });
 
-//dahlia's pick 토글
-const dahliaPickToggleEls = document.querySelectorAll('.dahlia-pick__titles h3');
+//dahlia's pick tab
+const pickTabEls = document.querySelectorAll('.dahlia-pick__titles h3');
 
-dahliaPickToggleEls.forEach(function(dahliaPickToggleEl, index) {
-  dahliaPickToggleEl.addEventListener('click', function () {
-    dahliaPickToggleEls.forEach(function (pickEl) {
-      pickEl.classList.remove('active');
-    })
-    dahliaPickToggleEl.classList.add('active');
-    // if(dahliaPickToggleEl.classList.contains('active')!=true) {
-    //   dahliaPickToggleEl.classList.remove('active');
+pickTabEls.forEach(function(pickTabEl, index) {
+  pickTabEl.addEventListener('click', function () {
+    pickTabEls.forEach(function (pickTabEl) {
+      pickTabEl.classList.remove('active');
+    });
+    // TODO 전체 지우기 Q.22-12-27혹시 숫자 전체를 선택하는 연산자 * 같은거나 배열의 길이를 콜백하는 방법은?
+    for (n=1;n<=2;n++){
+    document.querySelectorAll('.pick-'+ n).forEach(function(pickEl) {
+      pickEl.classList.remove('active');      
+    });
+    }
+    // 누른 요소만 active하는 방법
+    // pickTabEl.classList.add('active');
+
+    // 차차의 처음생각? --지우기--
+    // if(pickTabEl.classList.contains('active')!=true) {
+    //   pickTabEl.classList.remove('active');
     // }
+    
 
-    document.querySelectorAll('.pick-' + (index + 1));
+    // 누른요소pick-n 선택하고 active 클래스 붙이기
+    document.querySelectorAll('.pick-' + (index + 1)).forEach(function(pickEl,index) {
+      pickEl.classList.add('active');    
+  })
+    
   });
 })
