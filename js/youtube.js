b60f0817e228861197256c936b80a7d56336e138
jenny.cha <chajg91@naver.com>
2022-12-27 16:49:24 +0900

    7. day25-youtube영역-작업중
    배치는 간단히 했지만
    태블릿,모바일에서 반응형으로 작동 하기위해 css와 js를 수정해야 함

--- js/youtube.js
+++ js/youtube.js
@@ -9,14 +9,15 @@ var tag = document.createElement('script');
       // var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          // height: '360',
-          // width: '640',
-          videoId: 'csPjJQ7c-yE', // 최초 재생할 유튜브 영상 ID
+          height: '602',
+          width: '1070',
+          videoId: 'b8Si68tcpCA', // 최초 재생할 유튜브 영상 ID
           playerVars: { // 자세한 옵션은 플레이어 매개변수 메뉴 확인
             autoplay: true, // 자동 재생 유무
             loop: true, // 반복 재생 유무 (아래 플레이리스트 옵션 필수 영상이 하나일 지라도 반복재생 하고 싶으면 playlist를 꼭 채워줘야한다.)
-            playlist: ['csPjJQ7c-yE','M7lc1UVf-VE'], // 반복 재생할 유튜브 영상 ID 목록
+            playlist: ['b8Si68tcpCA','l4hQ7udfEuM'], // 반복 재생할 유튜브 영상 ID 목록
           },
+          // align: middle,
           events: {
             // 'onReady': onPlayerReady,
             // 'onStateChange': onPlayerStateChange
